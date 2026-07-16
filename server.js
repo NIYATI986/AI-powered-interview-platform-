e03356810b868475e25e5c3bb15e2562969c3f4c
Prachi-Singh-CSE <prachisingh35045@gmail.com>
2026-07-16 12:17:20 +0530

    Setup backend and MongoDB connection

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 
+
+
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./db");  
 
 const app = express();
+
+// Connect Database
+connectDB();  
 
 // Middleware
 app.use(cors());
